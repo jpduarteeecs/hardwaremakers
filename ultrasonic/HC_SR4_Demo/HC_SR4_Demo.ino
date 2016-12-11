eb6e87218a0cabbcfb9c354910a0336f53c4b981
jpduarte <jpduarte@berkeley.edu>
2016-12-11 02:47:27 -0800

    trying directly with arduino code

--- ultrasonic/HC_SR4_Demo/HC_SR4_Demo.ino
+++ ultrasonic/HC_SR4_Demo/HC_SR4_Demo.ino
@@ -3,27 +3,56 @@
 SYSTEM_MODE(MANUAL);
 #endif
 
-#include "Ultrasonic.h"
 
+
+
+
+/*
+HC-SR04 Ping distance sensor]
+VCC to arduino 5v GND to arduino GND
+Echo to Arduino pin 13 Trig to Arduino pin 12
+Red POS to Arduino pin 11
+Green POS to Arduino pin 10
+560 ohm resistor to both LED NEG and GRD power rail
+More info at: http://goo.gl/kJ8Gl
+Original code improvements to the Ping sketch sourced from Trollmaker.com
+Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+*/
 #define trigPin D6 // Trig pin on the HC-SR04
 #define echoPin D5 // Echo pin on the HC-SR04
-
-Ultrasonic ultrasonic(trigPin,echoPin);
+#define led 11
+#define led2 10
 
 void setup() {
-Serial.begin(9600);
-delay(1000);
-
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 
-void loop()
-{
-  //lcd.clear();
-  Serial.printf("Ultrasonic: %d\n",ultrasonic.Ranging(CM));
-  delay(100);
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance < 4) {  // This is where the LED On/Off happens
+    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+  digitalWrite(led2,LOW);
 }
-
-
+  else {
+    digitalWrite(led,LOW);
+    digitalWrite(led2,HIGH);
+  }
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(500);
+}
 
 
 
