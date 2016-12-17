65f31d3ed22123d5d8285848317aad456dc7b258
jpduarte <jpduarte@berkeley.edu>
2016-12-17 01:25:46 -0800

    try code from japanase maker, then update it to redbear duo, it compiles but numbers dont make sense

---
+++ accelerometer_gyro/accelerometer_gyro_example/accelerometer_gyro_example.ino
@@ -0,0 +1,84 @@
+/* Graph I2C Accelerometer On RedBear Duo over Serial Port
+ * Adafruit Part 2809 LIS3DH - http://adafru.it/2809
+ * This example shows how to program I2C manually
+ * I2C Pins SDA1==D0, SCL1 == D1
+ * Default address: 0x18
+ */
+
+// do not use the cloud functions - assume programming through Arduino IDE
+#if defined(ARDUINO)
+SYSTEM_MODE(MANUAL);
+#endif
+// Basic demo for accelerometer readings from Adafruit LIS3DH
+
+//#include <BLE_API.h>
+//#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+
+//BLE ble;
+//Timeout timeout;
+MPU6050 mpu;
+
+#define TXRX_BUF_LEN 20
+
+static uint8_t tx_buf[TXRX_BUF_LEN];
+
+
+uint8_t tx_value[TXRX_BUF_LEN] = {0,};
+uint8_t rx_value[TXRX_BUF_LEN] = {0,};
+
+
+
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+
+void setup() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
+
+  Serial.begin(250000);
+
+  // initialize device
+  Serial.println("Initializing I2C devices...");
+  accelgyro.initialize();
+
+  // verify connection
+  Serial.println("Testing device connections...");
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+
+  /*mpu.setXAccelOffset(-1020);
+  mpu.setYAccelOffset(-1030);
+  mpu.setZAccelOffset(1470);
+  mpu.setXGyroOffset(5000);
+  mpu.setYGyroOffset(900);
+  mpu.setZGyroOffset(200);*/
+
+
+  Serial.println("Advertising Start!");
+}
+
+void loop() {
+  // read raw accel/gyro measurements from device
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+
+  Serial.print("a/g:\t");
+  Serial.print(ax); Serial.print("\t");
+  Serial.print(ay); Serial.print("\t");
+  Serial.print(az); Serial.print("\t");
+  Serial.print(gx); Serial.print("\t");
+  Serial.print(gy); Serial.print("\t");
+  Serial.println(gz);
+  delay(100);
+
+}
