3f8ba01b2357a79d3bec2d57dbd86912807198fa
jpduarte <jpduarte@berkeley.edu>
2017-01-19 15:37:35 -0800

    clean up sound file

--- python-board/sound/soundexample.py
+++ python-board/sound/soundexample.py
@@ -4,7 +4,7 @@ from pydub.playback import play
 from time import sleep
 
 path = "/home/juan/research/hardwaremakers/python-board/sound/"
-#fname = "/home/juan/Downloads/Angklung.mp3"
+
 #Mina Azhar (Juan's wife) did all the mp3 conversion, thank you!
 fname = path+"low_do_long.mp3"
 dol = AudioSegment.from_mp3(fname)
@@ -22,25 +22,10 @@ fname = path+"si_long.mp3"
 si = AudioSegment.from_mp3(fname)
 fname = path+"high_do_long.mp3"
 doh = AudioSegment.from_mp3(fname)
-#play(do+re+mi+mi+mi+fa)
-#print(do.duration_seconds)
-#play(dol+re+mi+fa+so+la+si+doh)
+
 print(re.duration_seconds)
 play(dol+re[:int(re.duration_seconds*1000/3)]+mi+dol[:int(dol.duration_seconds*1000/3)]+mi[:int(dol.duration_seconds*1000/2)]+dol[:int(dol.duration_seconds*1000/2)]+mi)
 #dol+a+deer,+a+female+deer+
 sleep(.3)
 play(re+mi[:int(mi.duration_seconds*1000/3)]+fa[:int(fa.duration_seconds*1000/3)]+fa[:int(fa.duration_seconds*1000/3)]+mi[:int(mi.duration_seconds*1000/3)]+re[:int(re.duration_seconds*1000/3)]+fa)
 #re+a+drop+of+golden+sun+
-'''play(mi+fa+so+mi+so+mi+so)
-#me+a+name+i+call+myself+
-play(fa+so+la+la+so+fa+la)
-#fa,+a+long+long+way+to+run+
-play(so+dol+re+mi+fa+so+la)
-#sew,+a+needle+pulling+thread+
-play(la+re+mi+fa+so+la+si)
-#la,+a+note+to+follow+sol+
-play(si+mi+fa+so+la+si+doh)
-#ti,+a+drink+with+jam+and+bread+
-play(si+la+la+fa+si+so+doh+so+mi+re)
-#and+that+brings+us+back+to+dol.
-'''
