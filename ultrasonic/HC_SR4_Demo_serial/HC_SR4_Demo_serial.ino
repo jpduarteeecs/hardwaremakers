2bd3a88af505a42256f478ceb1fc4735d3b75e1f
jpduarte <jpduarte@berkeley.edu>
2017-01-24 12:23:18 -0800

    add example with serial code

---
+++ ultrasonic/HC_SR4_Demo_serial/HC_SR4_Demo_serial.ino
@@ -0,0 +1,26 @@
+//code to test HC-SR4 using Oscilloscope
+// Juan Duarte, January 2016
+
+#if defined(ARDUINO)
+SYSTEM_MODE(MANUAL);
+#endif
+
+#define trigPin D6 // Trig pin on the HC-SR04
+#define echoPin D5 // Echo pin on the HC-SR04
+
+unsigned long distance;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(trigPin, LOW);
+  distance = pulseIn(echoPin,HIGH);
+  Serial.println(distance);
+  delay(500);
+}
