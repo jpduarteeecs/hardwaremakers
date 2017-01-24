61574b61910ebe276427ae782974d7b6180596d5
jpduarte <jpduarte@berkeley.edu>
2017-01-24 12:12:35 -0800

    simple example for HR-SR4 working with signal from board

--- ultrasonic/HC_SR4_Demo/HC_SR4_Demo.ino
+++ ultrasonic/HC_SR4_Demo/HC_SR4_Demo.ino
@@ -1,91 +1,20 @@
-//https://www.hackster.io/hypnopompia/internet-controlled-adjustable-height-standing-desk-abd6e8
+//code to test HC-SR4 using Oscilloscope
+// Juan Duarte, January 2016
+
 #if defined(ARDUINO)
 SYSTEM_MODE(MANUAL);
 #endif
 
-
-
-
-
-/*
-HC-SR04 Ping distance sensor]
-VCC to arduino 5v GND to arduino GND
-Echo to Arduino pin 13 Trig to Arduino pin 12
-Red POS to Arduino pin 11
-Green POS to Arduino pin 10
-560 ohm resistor to both LED NEG and GRD power rail
-More info at: http://goo.gl/kJ8Gl
-Original code improvements to the Ping sketch sourced from Trollmaker.com
-Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
-*/
 #define trigPin D6 // Trig pin on the HC-SR04
-#define echoPin D5 // Echo pin on the HC-SR04
-
-boolean state = true;
-unsigned long time1,time2, distance;
 
 void setup() {
-  //Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-
-//  attachInterrupt(echoPin, isr, CHANGE); //CHANGE, RISING or FALLING
 }
 
 void loop() {
-
-  /*if(state) {
-     digitalWrite(trigPin, LOW);
-     state = false;
-  } else {
-    digitalWrite(trigPin, HIGH);
-    state = true;
-  }*/
-
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(500);
+  delay(5);
   digitalWrite(trigPin, LOW);
-  //Serial.println(state);
-  //delayMicroseconds(4500);
-  //time1 = micros(); //start or reset timer on every rising edge
-  distance = pulseIn(echoPin,HIGH);
-  Serial.println(distance);
-  delay(500);
-
-  //duration = pulseIn(echoPin, HIGH);
-  /*long duration, distance;
-  digitalWrite(trigPin, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(trigPin, HIGH);
-//  delayMicroseconds(1000); - Removed this line
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
-  if (distance < 4) {  // This is where the LED On/Off happens
-    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
-  digitalWrite(led2,LOW);
+  delay(5);
 }
-  else {
-    digitalWrite(led,LOW);
-    digitalWrite(led2,HIGH);
-  }
-  Serial.print(distance);
-  Serial.println(" cm");
-  delay(500);*/
-
-}
-
-/*void isr()
-{
-  if(digitalRead(echoPin)==HIGH){
-    time1 = micros(); //start or reset timer on every rising edge
-    distance = time2;
-  } else {
-   time2 = micros(); //stop on falling edge
-   distance = time2;
-  }
-  //interrupts();
-
-}
-*/
